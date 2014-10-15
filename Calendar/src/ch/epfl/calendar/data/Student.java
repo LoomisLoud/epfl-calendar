81352f2e678101c82445b48c2fe689a3c59e8de9
Enea Bell <enea.bell@epfl.ch>
2014-10-15 19:25:48 +0200

    Added sciper to student class

--- Calendar/src/ch/epfl/calendar/data/Student.java
+++ Calendar/src/ch/epfl/calendar/data/Student.java
@@ -7,9 +7,12 @@ import java.lang.reflect.Array;
  *
  */
 public class Student extends Person implements StudentData {
-
+	
+	public static final int MIN_LENGTH_SCIPER = 5;
+	public static final int MAX_LENGTH_SCIPER = 8;
 	//TODO Choose type of arguments
 	private String mStudentTimetable;
+	private int mSciper;
 	
 	//constructor needs specific argument to fill the data of the object
 	//TODO add arguments needed
@@ -20,5 +23,20 @@ public class Student extends Person implements StudentData {
 	public Array getLectureList() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public int getSCIPER() {
+		return mSciper;
+	}
+
+	public void setSCIPER(int sciper) {
+		//check if argument is correct
+		//get "length" of int (as number of digit)
+		int length = (int) (Math.log10(sciper)+1);
+		if (length < MIN_LENGTH_SCIPER || length > MAX_LENGTH_SCIPER || sciper < 0) {
+			//TODO could be changed to custom exception to better discribe
+			throw new IllegalArgumentException();
+		}
+		this.mSciper = sciper;
 	}
 }
