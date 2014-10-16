9c36d66a254c73b7acb02a39b6a14a6d3fd8092a
Marc Schaer <marc110191@gmail.com>
2014-10-16 22:26:22 +0200

    Changed way to read an URL, doesn't work

--- Calendar/src/ch/epfl/calendar/MainActivity.java
+++ Calendar/src/ch/epfl/calendar/MainActivity.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
@@ -11,8 +12,6 @@ import java.net.URL;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
-
 
 import android.app.Activity;
 import android.os.AsyncTask;
@@ -27,69 +26,87 @@ import android.view.MenuItem;
  *
  */
 public class MainActivity extends Activity {
-
+    private static final int READ_TIMEOUT = 7000;
+    private static final int CONNECT_TIMEOUT = 8000;
     private static final String TAG = "MainActivity";
+    private static final int BUFFER_LENGTH = 100000;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-		new FetchRandomQuestionTask().execute();
+		new FetchInformations().execute();
       
     }
 
     public String doInternetStuff() {
-    	String sUrl = "https://isa.epfl.ch/services/gps/EDOC";
+        String sUrl = "https://isa.epfl.ch/services/pedaperiods";
         URL url = null;
-		try {
-			url = new URL(sUrl);
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		HttpURLConnection httpURLConnection = null;
         try {
-			httpURLConnection = (HttpURLConnection) url.openConnection();
-			httpURLConnection.setReadTimeout(7000);
-	    	httpURLConnection.setConnectTimeout(8000);
-			httpURLConnection.setRequestMethod("GET");
-			httpURLConnection.addRequestProperty("Accept", "application/json");
-			//httpURLConnection.addRequestProperty("Accept", "application/json");
-			//start query - update, this is implicitly done by getInputStream()
-	    	//httpURLConnection.connect();
-	    	InputStream inputStreamObject = httpURLConnection.getInputStream();
-	    	// Convert the InputStream into a string
-	        BufferedReader streamReader = new BufferedReader(new InputStreamReader(inputStreamObject, "UTF-8"));
-	        StringBuilder responseStrBuilder = new StringBuilder();
+            url = new URL(sUrl);
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        HttpURLConnection httpURLConnection = null;
+        try {
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setReadTimeout(READ_TIMEOUT);
+            httpURLConnection.setConnectTimeout(CONNECT_TIMEOUT);
+            httpURLConnection.setRequestMethod("GET");
+            httpURLConnection.addRequestProperty("Accept", "application/json");
+            //start query - update, this is implicitly done by getInputStream()
+            //httpURLConnection.connect();
+            InputStream inputStreamObject = httpURLConnection.getInputStream();
+            // Convert the InputStream into a string
+            //BufferedReader streamReader = new BufferedReader(new InputStreamReader(inputStreamObject, "UTF-8"));
+            //StringBuilder responseStrBuilder = new StringBuilder();
+            String contentAsString = readInput(inputStreamObject, BUFFER_LENGTH);
+            System.out.println(contentAsString);
+            
+            /*while ((inputStr = streamReader.readLine()) != null) {
+                responseStrBuilder.append(inputStr);
+            }*/
+            Log.d(TAG, contentAsString);
 
-	        String inputStr;
-	        while ((inputStr = streamReader.readLine()) != null) {
-				responseStrBuilder.append(inputStr);
-			}
+            //JSONObject json = new JSONObject(responseStrBuilder.toString()+"}");
+            JSONObject json = new JSONObject(contentAsString);
 
-            Log.d(TAG, responseStrBuilder.toString());
-	        
-	        JSONObject json = new JSONObject(responseStrBuilder.toString());
+            //close
+            //inputStreamObject.close();
+            //streamReader.close();
+            httpURLConnection.disconnect();
+            //System.out.println(json.toString());
+            //Log.d(TAG, json.toString());
+            return json.toString();
 
-	        
-	        //close
-	        //inputStreamObject.close();
-	        streamReader.close();
-	        httpURLConnection.disconnect();
-	        Log.d(TAG, json.toString());
-	        return json.toString();
-
-		} catch (ProtocolException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        } catch (ProtocolException e) {
+        // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         return null;
+    }
+    
+    /**
+     * Read an InputStream and returns the content as a String
+     * @param inputStream
+     * @param bufferLen
+     * @return
+     * @author AblionGE
+     * @throws IOException 
+     */
+    private String readInput(InputStream inputStream, int bufferLen) throws IOException {
+        Reader reader = null;
+        reader = new InputStreamReader(inputStream, "UTF-8");
+        char[] buffer = new char[bufferLen];
+        reader.read(buffer);
+        return new String(buffer);
     }
 
     @Override
@@ -110,16 +127,21 @@ public class MainActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
-    private class FetchRandomQuestionTask extends AsyncTask<String, Void, String> {
-		
-		@Override
-		protected String doInBackground(String... params) {
-			return doInternetStuff();
-		}
-		
-		@Override
-		protected void onPostExecute(String result) {
-			//Log.i("result request", result);
-		}
-	}
+    /**
+     * 
+     * @author AblionGE
+     *
+     */
+    private class FetchInformations extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            return doInternetStuff();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            //Log.i("result request", result);
+        }
+    }
 }
