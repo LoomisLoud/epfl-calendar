5c1fae3198ca39a16b8e831f7abb71d7eada1326
Gil Brechbuhler <Gil.Brechbuhler@epfl.ch>
2014-10-16 20:16:05 +0200

    Create CalendarClient interface

---
+++ Calendar/src/ch/epfl/calendar/CalendarClient.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package ch.epfl.calendar;
+
+/**
+ * A client to get student's and classes' informations. This interfaces is meant to abstract the underlying
+ * network protocol and data formats.
+ * 
+ * @author gilbrechbuhler
+ *
+ */
+public interface CalendarClient {
+    
+}
