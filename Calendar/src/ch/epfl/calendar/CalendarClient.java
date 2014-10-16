502c0f0e5574b369456b2e617cab11651eeb89ef
Gil Brechbuhler <Gil.Brechbuhler@epfl.ch>
2014-10-16 21:06:54 +0200

    Add fetchLecture method in CalendarClient

--- Calendar/src/ch/epfl/calendar/CalendarClient.java
+++ Calendar/src/ch/epfl/calendar/CalendarClient.java
@@ -7,9 +7,26 @@ package ch.epfl.calendar;
  * A client to get student's and classes' informations. This interfaces is meant to abstract the underlying
  * network protocol and data formats.
  * 
+ * Every method listed in this class is temporary. These temporary methods will be modified when we now how things
+ * work with ISA.
+ * 
  * @author gilbrechbuhler
  *
  */
 public interface CalendarClient {
     
+    /**
+     * 
+     * @param username
+     * @param password
+     * @return
+     */
+    void login(String username, String password);
+    
+    /**
+     * 
+     * @param student
+     * @return
+     */
+    List<Lecture> fetchLectures(Student student);
 }
