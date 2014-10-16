910f0703b8108236dd70c83d9873544f30c3d4fc
Marc Schaer <marc110191@gmail.com>
2014-10-16 12:32:24 +0200

    Revert "Added example code to get data from html"
    This reverts commit 6fb27e775e7410e9c622c5e2220b511a813bdab4.

--- Calendar/src/ch/epfl/calendar/MainActivity.java
+++ Calendar/src/ch/epfl/calendar/MainActivity.java
@@ -1,18 +1,21 @@
 package ch.epfl.calendar;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 import android.app.Activity;
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -23,16 +26,59 @@ import android.view.MenuItem;
  */
 public class MainActivity extends Activity {
 
-    private static final String URL_INFO = "https://isa.epfl.ch/services/plans/2013-2014/semester/MA1/section/IN";
-	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        this.fetchPage();
+        //String sUrl = "https://isa.epfl.ch/services/gps/EDOC";
+        String sUrl = "http://www.google.com";
+        URL url = null;
+		try {
+			url = new URL(sUrl);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		HttpURLConnection httpURLConnection = null;
+        try {
+			httpURLConnection = (HttpURLConnection) url.openConnection();
+			httpURLConnection.setReadTimeout(7000);
+	    	httpURLConnection.setConnectTimeout(8000);
+			httpURLConnection.setRequestMethod("GET");
+			//start query - update, this is implicitly done by getInputStream()
+	    	//httpURLConnection.connect();
+	    	InputStream inputStreamObject = httpURLConnection.getInputStream();
+	    	// Convert the InputStream into a string
+	        BufferedReader streamReader = new BufferedReader(new InputStreamReader(inputStreamObject, "UTF-8"));
+	        StringBuilder responseStrBuilder = new StringBuilder();
+
+	        String inputStr;
+	        while ((inputStr = streamReader.readLine()) != null) {
+				responseStrBuilder.append(inputStr);
+			}
+
+	       /* JSONObject json = new JSONObject(responseStrBuilder.toString());
+	        
+	        //close
+	        //inputStreamObject.close();
+	        streamReader.close();
+	        httpURLConnection.disconnect();
+	        
+	        Log.i("Json value", json.toString());*/
+		} catch (ProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} /*catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+      
     }
 
+ 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -51,39 +97,5 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-    
-    public void fetchPage() {
-        ConnectivityManager connMgr = (ConnectivityManager) 
-                getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isConnected()) {
-            new DownloadWebpageTask().execute(URL_INFO);
-        }
-    }
-    
-    /**
-     * 
-     * @author AblionGE
-     *
-     */
-    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(String... urls) {
-            
-            Document doc = null;
-            try {
-                doc = Jsoup.connect(urls[0]).userAgent("Mozilla/5.0 Gecko/20100101 Firefox/21.0").get();
-                //TODO : cssQuery to have the wanted fields
-                Elements elements = doc.select("*");
-                for (Element element : elements) {
-                    System.out.println(element.text());
-                }
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            return doc.data();
-        }
     }
 }
