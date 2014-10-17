9bae4e06b4e82811d120d3956082a032453f85cb
Maxime Coriou <maxime.coriou@epfl.ch>
2014-10-17 17:10:07 +0200

    Linked course list to course details

--- Calendar/src/ch/epfl/calendar/CourseDetailsActivity.java
+++ Calendar/src/ch/epfl/calendar/CourseDetailsActivity.java
@@ -1,7 +1,9 @@
 package ch.epfl.calendar;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * @author LoomisLoud
@@ -13,5 +15,16 @@ public class CourseDetailsActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_course_details);
+		
+		// get the intent that started the Activity
+        Intent startingIntent = getIntent();
+        
+        // get the input string
+        String courseName =
+                startingIntent.getStringExtra(CoursesListActivity.class.getName());
+        
+        // get the TextView and update it
+        TextView textView = (TextView) findViewById(R.id.courseName);
+        textView.setText(courseName);
 	}
 }
--- Calendar/src/ch/epfl/calendar/CoursesListActivity.java
+++ Calendar/src/ch/epfl/calendar/CoursesListActivity.java
@@ -3,7 +3,11 @@ package ch.epfl.calendar;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -23,7 +27,7 @@ public class CoursesListActivity extends Activity {
 
         mListView = (ListView) findViewById(R.id.coursesListView);
 
-        ArrayList<String> coursesListArray = new ArrayList<String>();
+        final ArrayList<String> coursesListArray = new ArrayList<String>();
         coursesListArray.add("Cours 1");
         coursesListArray.add("Cours 2");
         coursesListArray.add("Cours 3");
@@ -32,5 +36,26 @@ public class CoursesListActivity extends Activity {
                 android.R.layout.simple_list_item_1, coursesListArray);
 
         mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                    int position, long id) {
+                openCourseDetails((String) mListView.getItemAtPosition(position));
+
+            }
+
+        });
+
+    }
+
+    public void openCourseDetails(String course) {
+        Intent courseDetailsActivityIntent = new Intent(this,
+                CourseDetailsActivity.class);
+
+        courseDetailsActivityIntent.putExtra(this.getClass().getName(), course);
+
+        startActivity(courseDetailsActivityIntent);
     }
 }
