66f6a5df0218509d3e629905ce5959f799edb555
Laurent Weingart <laurent.weingart@epfl.ch>
2014-10-18 00:42:04 +0200

    creates a package test for authentication

---
+++ CalendarTest/src/ch/epfl/calendar/test/authentication/AuthenticationActivityTest.java
@@ -0,0 +1,14 @@
+package ch.epfl.calendar.test.authentication;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AuthenticationActivityTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
