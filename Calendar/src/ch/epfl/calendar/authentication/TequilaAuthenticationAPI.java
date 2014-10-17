c3ee7bdcc93c04746645293da13ccfdde21790f3
Laurent Weingart <laurent.weingart@epfl.ch>
2014-10-17 15:06:35 +0200

    creates authentication package for Tequila auth

---
+++ Calendar/src/ch/epfl/calendar/authentication/TequilaAuthenticationAPI.java
@@ -0,0 +1,11 @@
+package ch.epfl.calendar.authentication;
+
+
+/**
+ * Basic wrapper for the Tequila authentication process
+ * @author lweingart
+ *
+ */
+public class TequilaAuthenticationAPI {
+
+}
---
+++ Calendar/src/ch/epfl/calendar/authentication/TequilaAuthenticationTask.java
@@ -0,0 +1,20 @@
+package ch.epfl.calendar.authentication;
+
+import android.os.AsyncTask;
+
+/**
+ * The main class that connects with the Tequila server and authenticates
+ * user credentials
+ * @author lweingart
+ *
+ */
+public class TequilaAuthenticationTask extends AsyncTask<Void, Void, String> {
+
+	@Override
+	protected String doInBackground(Void... params) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
