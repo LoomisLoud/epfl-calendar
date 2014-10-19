0e077a5365c49c11fd2a8f050886b57dde3c974d
Laurent Weingart <laurent.weingart@epfl.ch>
2014-10-19 22:24:26 +0200

    creates GLobalPreferences class in utils package

---
+++ Calendar/src/ch/epfl/calendar/utils/GlobalPreferences.java
@@ -0,0 +1,10 @@
+package ch.epfl.calendar.utils;
+
+/**
+ * Serves as a place for everyone to have access to the preferences of the app
+ * @author lweingart
+ *
+ */
+public class GlobalPreferences {
+
+}
