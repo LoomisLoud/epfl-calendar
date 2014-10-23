1741368e31533c776bf1e6c04d89e7df77ab0de1
Gil Brechbuhler <Gil.Brechbuhler@epfl.ch>
2014-10-23 19:10:32 +0200

    Change some author fields because there was '¨' which are badly supported by jenkins

--- Calendar/src/ch/epfl/calendar/data/StudentData.java
+++ Calendar/src/ch/epfl/calendar/data/StudentData.java
@@ -4,7 +4,7 @@ import java.lang.reflect.Array;
 
 /**
  * Give higher level methods to get information needed from a student
- * @author Enea Bell & Marc Shär
+ * @author Enea Bell
  *
  */
 public interface StudentData {
