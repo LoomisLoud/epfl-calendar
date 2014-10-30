b5bad7b66594eccdb6cd68b0eb68ff5b64009d9f
Marc Schaer <marc110191@gmail.com>
2014-10-30 22:55:14 +0100

    Fixed Checkstyle problems

--- Calendar/src/ch/epfl/calendar/MainActivity.java
+++ Calendar/src/ch/epfl/calendar/MainActivity.java
@@ -1,7 +1,5 @@
 package ch.epfl.calendar;
 
-import ch.epfl.calendar.apiInterface.AppEngineClient;
-import ch.epfl.calendar.apiInterface.DatabaseInterface;
 import ch.epfl.calendar.display.CoursesListActivity;
 import android.app.ActionBar;
 import android.app.Activity;
@@ -21,7 +19,7 @@ import android.widget.CalendarView.OnDateChangeListener;
  */
 public class MainActivity extends Activity {
     
-    CalendarView calendar;
+    private CalendarView mCalendar;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,47 +55,47 @@ public class MainActivity extends Activity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-        case R.id.action_courses_list:
-            switchToCoursesList();
-            return true;
-        case R.id.action_add_events:
-            Toast.makeText(this, "Not yet implemented", Toast.LENGTH_SHORT).show();
-            return true;
-        case R.id.action_draft_activity:
-            switchToDraftActivity();
-            return true;
-        case R.id.action_settings:
-            Toast.makeText(this, "Not yet implemented", Toast.LENGTH_SHORT).show();
-            return true;
-        default:
-            return super.onOptionsItemSelected(item);
+            case R.id.action_courses_list:
+                switchToCoursesList();
+                return true;
+            case R.id.action_add_events:
+                Toast.makeText(this, "Not yet implemented", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_draft_activity:
+                switchToDraftActivity();
+                return true;
+            case R.id.action_settings:
+                Toast.makeText(this, "Not yet implemented", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
     
     public void initializeCalendar() {
-        calendar = (CalendarView) findViewById(R.id.calendar);
+        mCalendar = (CalendarView) findViewById(R.id.calendar);
 
         // sets whether to show the week number.
-        calendar.setShowWeekNumber(false);
+        mCalendar.setShowWeekNumber(false);
 
         // sets the first day of week according to Calendar.
         // here we set Monday as the first day of the Calendar
-        calendar.setFirstDayOfWeek(2);
+        mCalendar.setFirstDayOfWeek(2);
 
         //The background color for the selected week.
-        calendar.setSelectedWeekBackgroundColor(getResources().getColor(R.color.green));
+        mCalendar.setSelectedWeekBackgroundColor(getResources().getColor(R.color.green));
         
         //sets the color for the dates of an unfocused month. 
-        calendar.setUnfocusedMonthDateColor(getResources().getColor(R.color.transparent));
+        mCalendar.setUnfocusedMonthDateColor(getResources().getColor(R.color.transparent));
     
         //sets the color for the separator line between weeks.
-        calendar.setWeekSeparatorLineColor(getResources().getColor(R.color.transparent));
+        mCalendar.setWeekSeparatorLineColor(getResources().getColor(R.color.transparent));
         
         //sets the color for the vertical bar shown at the beginning and at the end of the selected date.
-        calendar.setSelectedDateVerticalBar(R.color.darkgreen);
+        mCalendar.setSelectedDateVerticalBar(R.color.darkgreen);
         
         //sets the listener to be notified upon selected date change.
-        calendar.setOnDateChangeListener(new OnDateChangeListener() {
+        mCalendar.setOnDateChangeListener(new OnDateChangeListener() {
                        //show the selected date as a toast
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int day) {
