6cb9ff159c9045cb3ceed69452bc460653de8741
Marc Schaer <marc110191@gmail.com>
2014-10-30 23:10:51 +0100

    Fixed error CalendarClientTest - XML String

--- Calendar/src/ch/epfl/calendar/apiInterface/CalendarClient.java
+++ Calendar/src/ch/epfl/calendar/apiInterface/CalendarClient.java
@@ -28,8 +28,8 @@ public class CalendarClient implements CalendarClientInterface {
     @Override
     public List<Course> getISAInformations() throws CalendarClientException {
         /*****************************TEST XML PARSER*************************/
-        String contentAsString = "<data status=\"Termine\" date=\"20141017 16:08:36\""
-                + "key=\"1864682915\" dateFin=\"19.10.2014\" dateDebut=\"13.10.2014\"" 
+        String contentAsString = "<data status=\"Termine\" date=\"20141017 16:08:36\" "
+                + "key=\"1864682915\" dateFin=\"19.10.2014\" dateDebut=\"13.10.2014\">"
                 + "<study-period><id>1808047617</id><date>13.10.2014</date><duration>105</duration>"
                 + "<day>1</day><startTime>14:15</startTime><endTime>16:00</endTime>"
                 + "<type><text lang=\"en\">Lecture</text><text lang=\"fr\">Cours</text></type>"
@@ -43,8 +43,8 @@ public class CalendarClient implements CalendarClientInterface {
                 + "<code>GCA331</code><name><text lang=\"fr\">GC A3 31</text></name></room><room><id>1614950371</id>"
                 + "<code>GCD0386</code><name><text lang=\"fr\">GC D0 386</text></name></room><room><id>2189114</id>"
                 + "<code>GCB330</code><name><text lang=\"fr\">GC B3 30</text></name></room></study-period>"
-                + "<study-period><id>1808331964</id><date>14.10.2014</date><duration>105</duration><day>2</day><"
-                + "startTime>08:15</startTime><endTime>10:00</endTime><type><text lang=\"en\">Lecture</text>"
+                + "<study-period><id>1808331964</id><date>14.10.2014</date><duration>105</duration><day>2</day>"
+                + "<startTime>08:15</startTime><endTime>10:00</endTime><type><text lang=\"en\">Lecture</text>"
                 + "<text lang=\"fr\">Cours</text></type><course><id>24092923</id><name>"
                 + "<text lang=\"fr\">Software engineering</text></name></course><room><id>4255362</id>"
                 + "<code>BC02</code><name><text lang=\"fr\">BC 02</text></name></room><room><id>4255327</id>"
@@ -58,8 +58,10 @@ public class CalendarClient implements CalendarClientInterface {
         try {
             coursesList = ISAXMLParser.parse(new ByteArrayInputStream(contentAsString.getBytes("UTF-8")));
         } catch (XmlPullParserException e) {
+            System.out.println(contentAsString);
             throw new CalendarClientException();
         } catch (IOException e) {
+            System.out.println("IO");
             throw new CalendarClientException();
         }
         
