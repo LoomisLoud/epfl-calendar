f61c31c896988b213f005b92c24b33874f81aadf
Marc Schaer <marc110191@gmail.com>
2014-11-01 23:01:25 +0100

    Forgot some classes in merge

---
+++ Calendar/src/ch/epfl/calendar/data/StudentData.java
@@ -0,0 +1,13 @@
+package ch.epfl.calendar.data;
+
+import java.lang.reflect.Array;
+
+/**
+ * Give higher level methods to get information needed from a student
+ * @author Enea Bell
+ *
+ */
+public interface StudentData {
+
+	Array getLectureList();
+}
