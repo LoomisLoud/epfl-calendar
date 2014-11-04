03ba31aa45f0d2f14701196a1974e10dcfa637e1
Laurent Weingart <laurent.weingart@epfl.ch>
2014-11-04 11:17:22 +0100

    cleans httpClient factory of its warnings

--- Calendar/src/ch/epfl/calendar/authentication/HttpClientFactory.java
+++ Calendar/src/ch/epfl/calendar/authentication/HttpClientFactory.java
@@ -1,21 +1,16 @@
 package ch.epfl.calendar.authentication;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpResponseInterceptor;
-import org.apache.http.client.CookieStore;
 import org.apache.http.client.RedirectHandler;
 import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.conn.ssl.SSLSocketFactory;
-import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.AbstractHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.DefaultRedirectHandler;
@@ -73,40 +68,6 @@ public final class HttpClientFactory {
         }
     };
 
-    private static final RedirectHandler REDIRECT_FOLLOW = new RedirectHandler() {
-        @Override
-        public boolean isRedirectRequested(HttpResponse response, HttpContext context) {
-            return true;
-        }
-
-        @Override
-        public URI getLocationURI(HttpResponse response, HttpContext context) throws org.apache.http.ProtocolException {
-            return null;
-        }
-    };
-
-    private static final CookieStore COOKIE_MONSTER = new CookieStore() {
-        @Override
-        public void addCookie(Cookie cookie) {
-            /* do nothing */
-        }
-
-        @Override
-        public void clear() {
-            /* do nothing */
-        }
-
-        @Override
-        public boolean clearExpired(Date date) {
-            return true;
-        }
-
-        @Override
-        public List<Cookie> getCookies() {
-            return new ArrayList<Cookie>();
-        }
-    };
-
     private static final HttpRequestInterceptor LOGGING_REQUEST_INTERCEPTOR = new HttpRequestInterceptor() {
         @Override
         public void process(HttpRequest request, HttpContext context) {
@@ -129,7 +90,6 @@ public final class HttpClientFactory {
         ThreadSafeClientConnManager connManager = new ThreadSafeClientConnManager(params, schemeRegistry);
         AbstractHttpClient result = new DefaultHttpClient(connManager, params);
         result.setRedirectHandler(REDIRECT_NO_FOLLOW);
-        //result.setCookieStore(COOKIE_MONSTER);
         result.addRequestInterceptor(LOGGING_REQUEST_INTERCEPTOR);
         result.addResponseInterceptor(LOGGING_RESPONSE_INTERCEPTOR);
         return result;
