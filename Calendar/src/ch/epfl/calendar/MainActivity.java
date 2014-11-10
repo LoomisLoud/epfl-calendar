e1572ffb00a4fcd655012150d006be5c09096fce
Laurent Weingart <laurent.weingart@epfl.ch>
2014-11-10 16:35:52 +0100

    removes system print

--- Calendar/src/ch/epfl/calendar/MainActivity.java
+++ Calendar/src/ch/epfl/calendar/MainActivity.java
@@ -7,7 +7,6 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -161,11 +160,6 @@ public class MainActivity extends Activity {
         	e.printStackTrace();
         }
 
-        //To see if it works
-        Log.i(TAG, "list of courses:");
-        for (Course c : courses) {
-            System.out.println(c.toString());
-        }
         return courses;
     }
 
