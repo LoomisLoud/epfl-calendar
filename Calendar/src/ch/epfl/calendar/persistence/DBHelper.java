a1dec2e86ec078823251a6b14538f86784b779fe
Laurent Weingart <laurent.weingart@epfl.ch>
2014-11-15 16:05:21 +0100

    creates DBHelper class

---
+++ Calendar/src/ch/epfl/calendar/persistence/DBHelper.java
@@ -0,0 +1,35 @@
+/**
+ *
+ */
+package ch.epfl.calendar.persistence;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * @author lweingart
+ *
+ */
+public class DBHelper extends SQLiteOpenHelper {
+
+	public static final String DATABASE_NAME = "Calendar.db";
+	public static final int DATABASE_VERSION = 1;
+
+	public DBHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
