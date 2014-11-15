e6ccf6115ddc757c1fb462b2776478cef4e2a176
Laurent Weingart <laurent.weingart@epfl.ch>
2014-11-15 19:45:36 +0100

    creates period data source

---
+++ Calendar/src/ch/epfl/calendar/persistence/PeriodDataSource.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package ch.epfl.calendar.persistence;
+
+/**
+ * @author lweingart
+ *
+ */
+public class PeriodDataSource implements DAO {
+
+	/* (non-Javadoc)
+	 * @see ch.epfl.calendar.persistence.DAO#create(java.lang.Object)
+	 */
+	@Override
+	public void create(Object obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see ch.epfl.calendar.persistence.DAO#update(java.lang.Object)
+	 */
+	@Override
+	public void update(Object obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see ch.epfl.calendar.persistence.DAO#delete(java.lang.Object)
+	 */
+	@Override
+	public void delete(Object obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see ch.epfl.calendar.persistence.DAO#deleteAll()
+	 */
+	@Override
+	public void deleteAll() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
