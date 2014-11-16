2d06a54dd7c72de8e15df6b3e84831bca6731c68
Laurent Weingart <laurent.weingart@epfl.ch>
2014-11-16 13:39:23 +0100

    modifies dbhelper

--- Calendar/src/ch/epfl/calendar/persistence/DBHelper.java
+++ Calendar/src/ch/epfl/calendar/persistence/DBHelper.java
@@ -3,34 +3,26 @@
  */
 package ch.epfl.calendar.persistence;
 
+import ch.epfl.calendar.App;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /**
+ * Class for managing the persistent storage in DB
+ *
  * @author lweingart
  *
  */
 public class DBHelper extends SQLiteOpenHelper {
 
 	/**
-	 * Database file name.
+	 * Construct a new DBPoint entry.
+	 *
+	 * @param context
 	 */
-	public static final String DATABASE_NAME = "Calendar.db";
-
-	/**
-	 * Database version. This number must be increased whenever the database
-	 * schema is upgraded in order to trigger the
-	 * {@link SQLiteOpenHelper#onUpgrade(android.database.sqlite.SQLiteDatabase, int, int)}
-	 * method.
-	 */
-	public static final int DATABASE_VERSION = 1;
-
-	private Context mContext;
-
 	public DBHelper(Context context) {
-		super(context, DATABASE_NAME, null, DATABASE_VERSION);
-		mContext = context;
+		super(context, App.DATABASE_NAME, null, App.DATABASE_VERSION);
 	}
 
 	@Override
@@ -43,9 +35,5 @@ public class DBHelper extends SQLiteOpenHelper {
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		PeriodTable.onUpgrade(db, oldVersion, newVersion);
 		CourseTable.onUpgrade(db, oldVersion, newVersion);
-	}
-
-	public Context getContext() {
-		return this.mContext;
 	}
 }
