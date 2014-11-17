908e701db514a14c4153d54a8890054d0dca2868
Marc Schaer <marc110191@gmail.com>
2014-11-17 16:42:28 +0100

    Tried testing a part of AuthenticationActivity

--- CalendarTest/src/ch/epfl/calendar/authentication/tests/AuthenticationActivityTest.java
+++ CalendarTest/src/ch/epfl/calendar/authentication/tests/AuthenticationActivityTest.java
@@ -1,14 +1,46 @@
 package ch.epfl.calendar.authentication.tests;
 
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.*;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.*;
+import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.*;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.*;
+
+import org.robolectric.shadows.ShadowToast;
+
+import static org.hamcrest.Matchers.*;
+
+import com.google.android.apps.common.testing.ui.espresso.NoMatchingViewException;
+
+import android.content.Context;
+import android.test.ActivityInstrumentationTestCase2;
+import ch.epfl.calendar.R;
+import ch.epfl.calendar.authentication.AuthenticationActivity;
 
 /**
- * Class test for the Authentication Activity
+ * This class tests the AuthenticationActivity
  * @author AblionGE
  *
  */
-public class AuthenticationActivityTest {
+public class AuthenticationActivityTest
+    extends ActivityInstrumentationTestCase2<AuthenticationActivity> {
 
-	public void test() {
-	}
+    public AuthenticationActivityTest() {
+        super(AuthenticationActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        getActivity();
+    }
+
+    public final void testButtonIsDisplayedAndEnabled() {
+        onView(withId(R.id.btnLogin)).check(matches(isDisplayed()));
+        onView(withId(R.id.btnLogin)).check(matches(isEnabled()));
+    }
+
+    public final void testClickWithoutCredentials() {
+        onView(withId(R.id.btnLogin)).perform(click());
+    }
 
 }
