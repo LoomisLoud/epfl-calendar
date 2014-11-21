27777942d1cce8b231aba80676e64d655a11231c
Laurent Weingart <laurent.weingart@epfl.ch>
2014-11-21 23:14:11 +0100

    moves a test from a class to another

--- CalendarTest/src/ch/epfl/calendar/test/AppTest.java
+++ CalendarTest/src/ch/epfl/calendar/test/AppTest.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import android.util.Log;
 
 import ch.epfl.calendar.App;
+import ch.epfl.calendar.data.Period;
 
 import junit.framework.TestCase;
 
@@ -45,5 +46,111 @@ public class AppTest extends TestCase {
 		result.add("bar");
 		result.add("foobar");
 		return result;
+	}
+
+	public void testCreateCalendar() {
+		try {
+			//Null Period
+			new Period(null, null, null, null, null);
+			fail("Should have thrown NullPointerException");
+		} catch (NullPointerException e) {
+			//success
+		}
+
+		try {
+			//Bad format of date
+			new Period("", "16:15", "17:15", null, null);
+			fail("Should have thrown IllegalArgumentException");
+		} catch (IllegalArgumentException e) {
+			//success
+		}
+
+		try {
+			//Bad format of start date
+			new Period("16.10.2014", "", "17:15", null, null);
+			fail("Should have thrown IllegalArgumentException");
+		} catch (IllegalArgumentException e) {
+			//success
+		}
+
+		try {
+			//Bad format of end date
+			new Period("16.10.2014", "16:15", "", null, null);
+			fail("Should have thrown IllegalArgumentException");
+		} catch (IllegalArgumentException e) {
+			//success
+		}
+
+		try {
+			//Bad format of day
+			new Period("-1.10.2014", "16:15", "17:15", null, null);
+			fail("Should have thrown IllegalArgumentException");
+		} catch (IllegalArgumentException e) {
+			//success
+		}
+
+		try {
+			//Bad format of day
+			new Period("32.10.2014", "16:15", "17:15", null, null);
+			fail("Should have thrown IllegalArgumentException");
+		} catch (IllegalArgumentException e) {
+			//success
+		}
+
+		try {
+			//Bad format of month
+			new Period("16.00.2014", "16:15", "17:15", null, null);
+			fail("Should have thrown IllegalArgumentException");
+		} catch (IllegalArgumentException e) {
+			//success
+		}
+
+		try {
+			//Bad format of month
+			new Period("16.13.2014", "16:15", "17:15", null, null);
+			fail("Should have thrown IllegalArgumentException");
+		} catch (IllegalArgumentException e) {
+			//success
+		}
+
+		try {
+			//Bad format of year
+			new Period("16.01.1969", "16:15", "17:15", null, null);
+			fail("Should have thrown IllegalArgumentException");
+		} catch (IllegalArgumentException e) {
+			//success
+		}
+
+		try {
+			//Bad format of hour
+			new Period("16.01.2014", "-1:15", "17:15", null, null);
+			fail("Should have thrown IllegalArgumentException");
+		} catch (IllegalArgumentException e) {
+			//success
+		}
+
+		try {
+			//Bad format of hour
+			new Period("16.01.2014", "24:15", "17:15", null, null);
+			fail("Should have thrown IllegalArgumentException");
+		} catch (IllegalArgumentException e) {
+			//success
+		}
+
+		try {
+			//Bad format of minute
+			new Period("16.01.2014", "16:-1", "17:15", null, null);
+			fail("Should have thrown IllegalArgumentException");
+		} catch (IllegalArgumentException e) {
+			//success
+		}
+
+		try {
+			//Bad format of minute
+			new Period("16.01.2014", "16:60", "17:15", null, null);
+			fail("Should have thrown IllegalArgumentException");
+		} catch (IllegalArgumentException e) {
+			//success
+		}
 	}
 }
