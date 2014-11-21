8aa021bc248efed2cab7c5f6f5f66f22c878d5de
Gil Brechbuhler <Gil.Brechbuhler@epfl.ch>
2014-11-21 14:18:29 +0100

    Add javadoc to AuthenticationUtils

--- Calendar/src/ch/epfl/calendar/utils/AuthenticationUtils.java
+++ Calendar/src/ch/epfl/calendar/utils/AuthenticationUtils.java
@@ -12,11 +12,20 @@ import ch.epfl.calendar.authentication.TequilaAuthenticationAPI;
  */
 public class AuthenticationUtils {
     
+    /**
+     * 
+     * @param context
+     * @return true if the user is authenticated, false otherwise.
+     */
     public boolean isAuthenticated(Context context) {
         String tokenID = getTequilaAPI().getSessionID(context);
         return (tokenID != null) && !tokenID.isEmpty();
     }
     
+    /**
+     * 
+     * @return a TequilaAPI instance.
+     */
     public TequilaAuthenticationAPI getTequilaAPI() {
         return TequilaAuthenticationAPI.getInstance();
     }
