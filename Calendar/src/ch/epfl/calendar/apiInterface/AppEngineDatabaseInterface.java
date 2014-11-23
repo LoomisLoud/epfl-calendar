55e7c7ff2526fedf055dc6cf8ddb77f9edc59d95
Marc Schaer <marc110191@gmail.com>
2014-11-23 22:26:35 +0100

    Rename Database Interfaces

---
+++ Calendar/src/ch/epfl/calendar/apiInterface/AppEngineDatabaseInterface.java
@@ -0,0 +1,41 @@
+/**
+ *
+ */
+package ch.epfl.calendar.apiInterface;
+
+import ch.epfl.calendar.data.Course;
+
+/**
+ * This interface is used to access the database in a transparent way.
+ *
+ * @author gilbrechbuhler
+ *
+ */
+public interface AppEngineDatabaseInterface {
+
+    /**
+     *
+     * @param name The name of the course to fetch
+     * @return The Course object filled in with the course's informations.
+     * @throws CalendarClientException
+     */
+    Course getCourseByName(String name) throws CalendarClientException;
+
+    /**
+     *
+     * @param code The code of the course to fetch
+     * @return The course Object filled in with the course's informations
+     * @throws CalendarClientException
+     */
+    Course getCourseByCode(String code) throws CalendarClientException;
+
+    /*
+     *
+     * @param period The period to put in the database
+     * @param courseCode the code of the course of which the period belongs.
+     * @throws CalendarClientException
+     */
+
+//    void createPeriod(Period period, String courseCode) throws CalendarClientException;
+
+}
