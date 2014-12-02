e7cd841995549b70f59bbd33a31862a98156460e
Laurent Weingart <laurent.weingart@epfl.ch>
2014-12-02 09:49:54 +0100

    adds class comment

--- CalendarTest/src/ch/epfl/calendar/persistence/tests/CourseTableTest.java
+++ CalendarTest/src/ch/epfl/calendar/persistence/tests/CourseTableTest.java
@@ -1,14 +1,21 @@
 package ch.epfl.calendar.persistence.tests;
 
 import junit.framework.TestCase;
+import ch.epfl.calendar.App;
 
+/**
+ *
+ * @author lweingart
+ *
+ */
 public class CourseTableTest extends TestCase {
 
-	public void setup() {
-//        super.setUp();
+	@Override
+	public void setUp() {
+        super.setUp();
 
-//        App.setDBHelper("calendar_test.db");
-//        getInstrumentation().getTargetContext().deleteDatabase(
-//                App.getDBHelper().getDatabaseName());
+        App.setDBHelper("calendar_test.db");
+        getInstrumentation().getTargetContext().deleteDatabase(
+                App.getDBHelper().getDatabaseName());
 	}
 }
