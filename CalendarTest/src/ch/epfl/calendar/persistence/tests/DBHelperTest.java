ff34b120f80e296a39926265d807c90a6bd0c06a
Laurent Weingart <laurent.weingart@epfl.ch>
2014-12-02 11:40:56 +0100

    creates a test class for DBHelper class

---
+++ CalendarTest/src/ch/epfl/calendar/persistence/tests/DBHelperTest.java
@@ -0,0 +1,27 @@
+package ch.epfl.calendar.persistence.tests;
+
+import junit.framework.TestCase;
+import android.test.RenamingDelegatingContext;
+import ch.epfl.calendar.App;
+import ch.epfl.calendar.persistence.DBHelper;
+
+public class DBHelperTest extends TestCase {
+
+	private DBHelper mDBHelper;
+
+	@Override
+	public void setUp() {
+		RenamingDelegatingContext context = new RenamingDelegatingContext(App.getAppContext(), "test_db.db");
+		mDBHelper = new DBHelper(context, "test_db.db");
+	}
+
+	public void testAddEntry() {
+		// TODO some tests
+	}
+
+	@Override
+	public void tearDown() throws Exception {
+		mDBHelper.close();
+		super.tearDown();
+	}
+}
