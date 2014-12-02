398d04b7b2d84a8e2e46828847c64a6f0e6fa5b4
Laurent Weingart <laurent.weingart@epfl.ch>
2014-12-02 09:48:12 +0100

    creates package test persistence

---
+++ CalendarTest/src/ch/epfl/calendar/persistence/tests/CourseTableTest.java
@@ -0,0 +1,14 @@
+package ch.epfl.calendar.persistence.tests;
+
+import junit.framework.TestCase;
+
+public class CourseTableTest extends TestCase {
+
+	public void setup() {
+//        super.setUp();
+
+//        App.setDBHelper("calendar_test.db");
+//        getInstrumentation().getTargetContext().deleteDatabase(
+//                App.getDBHelper().getDatabaseName());
+	}
+}
