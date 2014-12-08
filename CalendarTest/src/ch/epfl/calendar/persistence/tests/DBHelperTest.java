dd98d1c0cf4d496918862b22c58f5aa961ae2c81
Marc Schaer <marc110191@gmail.com>
2014-12-08 11:22:03 +0100

    Close mDB

--- CalendarTest/src/ch/epfl/calendar/persistence/tests/DBHelperTest.java
+++ CalendarTest/src/ch/epfl/calendar/persistence/tests/DBHelperTest.java
@@ -31,6 +31,12 @@ public class DBHelperTest extends TestCase {
         App.setDBHelper("calendar_test.db");
         mDb = App.getDBHelper().getReadableDatabase();
     }
+    
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+        mDb.close();
+    }
 
     public void testTablesCreation() {
         String tableName = SQLiteDatabase
