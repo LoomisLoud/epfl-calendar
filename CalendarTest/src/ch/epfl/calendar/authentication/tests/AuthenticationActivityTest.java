1fd658f47c3b6ce574922ea67dfd4c1a655312a4
Laurent Weingart <laurent.weingart@epfl.ch>
2014-12-08 21:27:52 +0100

    removes useless method

--- CalendarTest/src/ch/epfl/calendar/authentication/tests/AuthenticationActivityTest.java
+++ CalendarTest/src/ch/epfl/calendar/authentication/tests/AuthenticationActivityTest.java
@@ -1,24 +1,23 @@
 package ch.epfl.calendar.authentication.tests;
 
-import static com.google.android.apps.common.testing.ui.espresso.Espresso.*;
-import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.*;
-import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.*;
-import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.*;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.typeText;
+import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isDisplayed;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isEnabled;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
 
-import android.content.Context;
 import android.test.ActivityInstrumentationTestCase2;
 import android.view.View;
 import android.widget.EditText;
 import ch.epfl.calendar.R;
 import ch.epfl.calendar.authentication.AuthenticationActivity;
-import ch.epfl.calendar.authentication.TequilaAuthenticationTask;
 
 /**
  * This class tests the AuthenticationActivity
@@ -29,7 +28,7 @@ public class AuthenticationActivityTest
     extends ActivityInstrumentationTestCase2<AuthenticationActivity> {
 
     private AuthenticationActivity mAuthActivity;
-    
+
     public AuthenticationActivityTest() {
         super(AuthenticationActivity.class);
     }
@@ -51,20 +50,20 @@ public class AuthenticationActivityTest
         onView(withId(R.id.txtUsername)).check(matches(isDisplayed()));
         onView(withId(R.id.txtPassword)).check(matches(isDisplayed()));
     }
-    
+
     public void testValueAtInit() {
         onView(withText(mAuthActivity.getString(R.string.login_submit_text))).check(matches(isDisplayed()));
         onView(withHint(mAuthActivity.getString(R.string.login_password_hint))).check(matches(isDisplayed()));
         onView(withHint(mAuthActivity.getString(R.string.login_username_hint))).check(matches(isDisplayed()));
     }
-    
+
     public void testFillFields() {
         onView(withId(R.id.txtUsername)).perform(typeText("test_username"));
         onView(withId(R.id.txtPassword)).perform(typeText("test_password"));
         onView(withId(R.id.txtUsername)).check(matches(withText("test_username")));
         onView(withId(R.id.txtPassword)).check(matches(withText("test_password")));
     }
-    
+
     private static Matcher<View> withHint(final String expectedHint) {
         return new TypeSafeMatcher<View>() {
 
@@ -84,13 +83,4 @@ public class AuthenticationActivityTest
             }
         };
     }
-    
-    private int getIdByName(String name) {
-        Context context = getInstrumentation().getTargetContext();
-        int result = context.getResources().getIdentifier(name, "id",
-                context.getPackageName());
-        assertTrue("id for name not found: " + name, result != 0);
-        return result;
-    }
-
 }
