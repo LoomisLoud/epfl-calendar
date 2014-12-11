98567cf2d11a87c043f8d97269ede12348d224e5
Enea Bell <enea.bell@epfl.ch>
2014-12-11 18:30:26 +0100

    Changed comment and checkstyle errors

--- CalendarTest/src/ch/epfl/calendar/display/tests/CourseDetailsActivityInstrumentationTest.java
+++ CalendarTest/src/ch/epfl/calendar/display/tests/CourseDetailsActivityInstrumentationTest.java
@@ -1,26 +1,20 @@
 package ch.epfl.calendar.display.tests;
 
-import static com.google.android.apps.common.testing.ui.espresso.Espresso.onData;
 import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
 import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
 import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
-import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.doesNotExist;
-import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isDisplayed;
-import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isEnabled;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.not;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import com.google.android.apps.common.testing.testrunner.ActivityLifecycleMonitorRegistry;
-import com.google.android.apps.common.testing.testrunner.Stage;
-import com.google.common.collect.Iterables;
-
+import android.app.Activity;
+import android.content.Intent;
+import android.test.ActivityInstrumentationTestCase2;
 import ch.epfl.calendar.App;
 import ch.epfl.calendar.R;
 import ch.epfl.calendar.data.Course;
@@ -31,9 +25,10 @@ import ch.epfl.calendar.persistence.DBQuester;
 import ch.epfl.calendar.persistence.LocalDatabaseInterface;
 import ch.epfl.calendar.testing.utils.MockActivity;
 import ch.epfl.calendar.testing.utils.Utils;
-import android.app.Activity;
-import android.content.Intent;
-import android.test.ActivityInstrumentationTestCase2;
+
+import com.google.android.apps.common.testing.testrunner.ActivityLifecycleMonitorRegistry;
+import com.google.android.apps.common.testing.testrunner.Stage;
+import com.google.common.collect.Iterables;
 
 /**
  * 
@@ -96,41 +91,12 @@ public class CourseDetailsActivityInstrumentationTest extends
         try {
             Utils.pressBack(getCurrentActivity());
         } catch (Throwable e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
         super.tearDown();
         getInstrumentation().getTargetContext().deleteDatabase(
                 App.getDBHelper().getDatabaseName());
     }
-
-    /*
-     * public void setUp() throws Exception { super.setUp();
-     * 
-     * /* SUPER DUPER IMPORTANT : class this next line in setUp in every test
-     * class that need access to the database !
-     */
-    /*
-     * App.setDBHelper("calendar_test.db");
-     * getInstrumentation().getTargetContext().deleteDatabase(
-     * App.getDBHelper().getDatabaseName());
-     * 
-     * mActivity = getActivity();
-     * 
-     * // We need to set up which activity is the current one (needed by //
-     * AsyncTask to be able to use callback functions
-     * App.setActionBar(mActivity); mActivity.setUdpateData(mActivity);
-     * 
-     * mDB = new DBQuester();
-     * 
-     * mDB.deleteAllTables(); mDB.createTables();
-     * 
-     * populateTestDB(); }
-     * 
-     * 
-     * 
-     * public void tearDown() throws Exception { super.tearDown(); }
-     */
 
     public void testLaunchActivityNothingInIntent() {
         setActivity();
@@ -149,7 +115,7 @@ public class CourseDetailsActivityInstrumentationTest extends
         onView(withId(R.id.courseName)).check(
                 matches(withText(mCourses.get(0).getName())));
     }
-    
+
     public void testAllMainTextViewVisible() {
         setIntentActivityWithExtra(mCourses.get(0).getName());
         onView(withId(R.id.courseName)).check(matches(isDisplayed()));
@@ -158,13 +124,13 @@ public class CourseDetailsActivityInstrumentationTest extends
         onView(withId(R.id.coursePeriod)).check(matches(isDisplayed()));
         onView(withId(R.id.courseDescription)).check(matches(isDisplayed()));
     }
-    
+
     public void testCourseWithNoDescription() {
         setIntentActivityWithExtra(mCourses.get(2).getName());
-        //no text should be shown
+        // no text should be shown
         onView(withId(R.id.courseDescription)).check(matches(withText("")));
     }
-    
+
     public void testSwitchActivityEvent() throws Throwable {
         setIntentActivityWithExtra(mCourses.get(0).getName());
         // current activity name
@@ -198,43 +164,6 @@ public class CourseDetailsActivityInstrumentationTest extends
         // setActivity
         setActivity();
     }
-
-    // private void populateTestDB() throws Exception {
-    // List<String> period1Course1Rooms = new ArrayList<String>();
-    // List<String> period2Course1Rooms = new ArrayList<String>();
-    // period1Course1Rooms.add("GCA 331");
-    // period1Course1Rooms.add("CO2");
-    // period2Course1Rooms.add("INF1");
-    // period2Course1Rooms.add("INF2");
-    // Period period1Course1 = new Period("Lecture", "27.11.2014 08:00",
-    // "27.11.2014 10:00", period1Course1Rooms, "1");
-    // Period period2Course1 = new Period("Exercise", "28.11.2014 08:00",
-    // "28.11.2014 10:00", period2Course1Rooms, "2");
-    // ArrayList<Period> periodsCourse1 = new ArrayList<Period>();
-    // periodsCourse1.add(period1Course1);
-    // periodsCourse1.add(period2Course1);
-    // Course course1 = new Course("TestCourse1", periodsCourse1,
-    // "Pr. Testpr1", 200, "CS-321", "awesome course", null);
-    //
-    // List<String> period1Course2Rooms = new ArrayList<String>();
-    // List<String> period2Course2Rooms = new ArrayList<String>();
-    // period1Course2Rooms.add("GCB 332");
-    // period1Course2Rooms.add("INF119");
-    // period2Course2Rooms.add("INM202");
-    // period2Course2Rooms.add("INM203");
-    // Period period1Course2 = new Period("Lecture", "30.11.2014 08:00",
-    // "30.11.2014 10:00", period1Course2Rooms, "3");
-    // Period period2Course2 = new Period("Exercise", "02.11.2014 08:00",
-    // "02.11.2014 10:00", period2Course2Rooms, "4");
-    // ArrayList<Period> periodsCourse2 = new ArrayList<Period>();
-    // periodsCourse2.add(period1Course2);
-    // periodsCourse2.add(period2Course2);
-    // Course course2 = new Course("TestCourse2", periodsCourse2,
-    // "Pr. Testpr2", 5, "CS-000", "cool course", null);
-    //
-    // mDB.storeCourse(course1);
-    // mDB.storeCourse(course2);
-    // }
 
     private void createCourses() throws Exception {
         List<String> period1Course1Rooms = new ArrayList<String>();
@@ -272,7 +201,6 @@ public class CourseDetailsActivityInstrumentationTest extends
         Course course2 = new Course("TestCourse2", periodsCourse2,
                 "Pr. Testpr2", 5, "CS-000", "cool course", null);
 
-        
         Course course3 = new Course("TestCourse3", periodsCourse2,
                 "Pr. Testpr3", 5, "CS-000", "", null);
         // Add events
