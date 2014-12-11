92404f716c7b169374dee763ea56bb706ebc5c8c
Marc Schaer <marc110191@gmail.com>
2014-12-11 18:47:24 +0100

    Fix checkstyle

--- Calendar/src/ch/epfl/calendar/display/AppEngineTask.java
+++ Calendar/src/ch/epfl/calendar/display/AppEngineTask.java
@@ -1,6 +1,5 @@
 package ch.epfl.calendar.display;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
 import ch.epfl.calendar.apiInterface.AppEngineClient;
